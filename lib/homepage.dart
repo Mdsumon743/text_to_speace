10e0e914ed22009fde6a3f89428bf2e2356b373b
mdsumon743 <mdsumonhossan743@gmail.com>
2024-12-09 16:44:56 +0600

    permission  done

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:just_audio/just_audio.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,6 +14,8 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   final FlutterTts flutterTts = FlutterTts();
+  List<FileSystemEntity> _musicFiles = [];
+  final audioPlayer = AudioPlayer();
   final TextEditingController textEditingController = TextEditingController();
   Map<String, String> languageMap = {
     'en-US': 'English',
@@ -18,9 +24,22 @@ class _HomepageState extends State<Homepage> {
   void initState() {
     super.initState();
     initTts();
+    _checkPermissions();
+  }
+
+  Future<void> playAudio(String filePath) async {
+    try {
+      await audioPlayer.setUrl(filePath);
+      await audioPlayer.play();
+    } catch (e) {
+      print('Error playing audio: $e');
+    }
   }
 
   List<String> languages = [];
+
+  bool _isLoading = false;
+
   String? selectedLanguage;
   double pitch = 1.0;
   double speachRate = 0.0;
@@ -53,7 +72,6 @@ class _HomepageState extends State<Homepage> {
     await flutterTts.setVolume(volume);
     await flutterTts.setSpeechRate(speachRate);
     String timespam = DateTime.now().microsecondsSinceEpoch.toString();
-
     await flutterTts.synthesizeToFile(text, 'audio_bite$timespam.mp3');
   }
 
@@ -65,92 +83,163 @@ class _HomepageState extends State<Homepage> {
     flutterTts.pause();
   }
 
+  Future<void> _checkPermissions() async {
+    PermissionStatus status = await Permission.manageExternalStorage.request();
+    if (status.isGranted) {
+      _loadMusicFiles();
+    } else {
+      print('Permission Denied');
+    }
+  }
+
+  Future<void> _loadMusicFiles() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      
+      final musicDir = Directory(
+          '/storage/emulated/0/Music'); 
+
+      
+      if (await musicDir.exists()) {
+       
+        final List<FileSystemEntity> files = musicDir.listSync();
+        setState(() {
+          _musicFiles =
+              files.where((file) => file.path.endsWith('.mp3')).toList();
+        });
+      } else {
+        debugPrint('Directory does not exist');
+      }
+    } catch (e) {
+      debugPrint('Error: $e');
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                TextField(
-                  maxLines: null,
-                  minLines: 3,
-                  controller: textEditingController,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(),
-                      enabledBorder: OutlineInputBorder()),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                        onPressed: () {
-                          setState(() {
-                            speak(textEditingController.text);
-                          });
-                        },
-                        icon: const Icon(
-                          Icons.play_arrow,
-                          color: Colors.greenAccent,
-                        )),
-                    IconButton(
-                        onPressed: () {
-                          setState(() {
-                            puase();
-                          });
-                        },
-                        icon: const Icon(
-                          Icons.pause,
-                          color: Colors.amber,
-                        )),
-                    IconButton(
-                        onPressed: () {
-                          setState(() {
-                            stop();
-                          });
-                        },
-                        icon: const Icon(
-                          Icons.stop,
-                          color: Colors.red,
-                        ))
-                  ],
-                ),
-                const Text('Volume '),
-                Slider(
-                  value: volume,
-                  onChanged: (value) {
-                    setState(() {
-                      volume = value;
-                    });
-                  },
-                ),
-                const Text('Pitch '),
-                Slider(
-                  value: pitch,
-                  onChanged: (value) {
-                    setState(() {
-                      pitch = value;
-                    });
-                  },
-                ),
-                const Text('Speach Rate '),
-                Slider(
-                  value: speachRate,
-                  onChanged: (value) {
-                    setState(() {
-                      speachRate = value;
-                    });
-                  },
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      save(textEditingController.text);
+        body: RefreshIndicator(
+          onRefresh: () async {
+            return setState(() {
+               initState();
+            });
+            
+           
+          },
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  TextField(
+                    maxLines: null,
+                    minLines: 3,
+                    controller: textEditingController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        focusedBorder: OutlineInputBorder(),
+                        enabledBorder: OutlineInputBorder()),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            setState(() {
+                              speak(textEditingController.text);
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.play_arrow,
+                            color: Colors.greenAccent,
+                          )),
+                      IconButton(
+                          onPressed: () {
+                            setState(() {
+                              puase();
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.pause,
+                            color: Colors.amber,
+                          )),
+                      IconButton(
+                          onPressed: () {
+                            setState(() {
+                              stop();
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.stop,
+                            color: Colors.red,
+                          ))
+                    ],
+                  ),
+                  const Text('Volume '),
+                  Slider(
+                    value: volume,
+                    onChanged: (value) {
+                      setState(() {
+                        volume = value;
+                      });
                     },
-                    child: const Text("save audio"))
-              ],
+                  ),
+                  const Text('Pitch '),
+                  Slider(
+                    value: pitch,
+                    onChanged: (value) {
+                      setState(() {
+                        pitch = value;
+                      });
+                    },
+                  ),
+                  const Text('Speach Rate '),
+                  Slider(
+                    value: speachRate,
+                    onChanged: (value) {
+                      setState(() {
+                        speachRate = value;
+                      });
+                    },
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        save(textEditingController.text);
+                      },
+                      child: const Text("save audio")),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  _isLoading
+                      ? const CircularProgressIndicator()
+                      : _musicFiles.isEmpty
+                          ? const Text('No MP3 files found')
+                          : ListView.builder(
+                              shrinkWrap: true,
+                              primary: false,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: _musicFiles.length,
+                              itemBuilder: (context, index) {
+                                String filePath = _musicFiles[index].path;
+                                String fileName = filePath.split('/').last;
+                                return ListTile(
+                                  title: Text(fileName),
+                                  onTap: () {
+                                    playAudio(filePath);
+                                  },
+                                );
+                              },
+                            ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,117 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:permission_handler/permission_handler.dart';
+// import 'package:just_audio/just_audio.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: MusicPlayerScreen(),
+//     );
+//   }
+// }
+
+// class MusicPlayerScreen extends StatefulWidget {
+//   const MusicPlayerScreen({super.key});
+
+//   @override
+//   // ignore: library_private_types_in_public_api
+//   _MusicPlayerScreenState createState() => _MusicPlayerScreenState();
+// }
+
+// class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
+//   List<FileSystemEntity> _musicFiles = [];
+//   final AudioPlayer _audioPlayer = AudioPlayer();
+//   bool _isLoading = false;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _checkPermissions();
+//   }
+
+//   Future<void> _checkPermissions() async {
+//     PermissionStatus status = await Permission.manageExternalStorage.request();
+//     if (status.isGranted) {
+//       _loadMusicFiles();
+//     } else {
+//       print('Permission Denied');
+//     }
+//   }
+
+//   Future<void> _loadMusicFiles() async {
+//     setState(() {
+//       _isLoading = true;
+//     });
+
+//     try {
+//       // For Android 10 and above, use external storage path
+//       final musicDir = Directory(
+//           '/storage/emulated/0/Music'); // Update with your correct path
+
+//       // Check if the directory exists
+//       if (await musicDir.exists()) {
+//         // Fetch all MP3 files in the directory
+//         final List<FileSystemEntity> files = musicDir.listSync();
+//         setState(() {
+//           _musicFiles =
+//               files.where((file) => file.path.endsWith('.mp3')).toList();
+//         });
+//       } else {
+//         print('Directory does not exist');
+//       }
+//     } catch (e) {
+//       print('Error: $e');
+//     } finally {
+//       setState(() {
+//         _isLoading = false;
+//       });
+//     }
+//   }
+
+//   Future<void> _playAudio(String filePath) async {
+//     try {
+//       await _audioPlayer.setUrl(filePath);
+//       _audioPlayer.play();
+//     } catch (e) {
+//       print('Error playing audio: $e');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Music Player'),
+//       ),
+//       body: Center(
+//         child: _isLoading
+//             ? const CircularProgressIndicator()
+//             : _musicFiles.isEmpty
+//                 ? const Text('No MP3 files found')
+//                 : ListView.builder(
+//                     itemCount: _musicFiles.length,
+//                     itemBuilder: (context, index) {
+//                       String filePath = _musicFiles[index].path;
+//                       String fileName = filePath.split('/').last;
+//                       return ListTile(
+//                         title: Text(fileName),
+//                         onTap: () {
+//                           _playAudio(filePath);
+//                         },
+//                       );
+//                     },
+//                   ),
+//       ),
+//     );
+//   }
+// }
